53f3eb2ccbbe71be8a461d95404c81603906a5b2
aditechdev <aditechdevofficial@gmail.com>
2021-05-30 11:18:58 +0530

    :art: Move the main.dart in lib folder
    Moveing main.dart to other folder asking for the new path, that should me change in launch.json by adding the line *            "program": "lib/your_class/file_name_with_main_function.dart"*

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
       ),
         body: Center(
           child: Container(
-            child: Text("Hello WOrld $days" ),
+            child: Text("Hello WOrld $days hii " ),
           ),
         ),
         drawer: Drawer(),
