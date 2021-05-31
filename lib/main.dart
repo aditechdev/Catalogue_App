e682648643dd323801eb4808b676129b5c4cd31c
aditechdev <aditechdevofficial@gmail.com>
2021-05-31 08:38:54 +0530

    :sparkles: Remove Debug Banner

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness: Brightness.dark
       ),
-      // debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
 
       initialRoute: "/",
 
